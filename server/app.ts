cba451f7b7881b966715db0bdf0b68e0c8401916
sergio15dev <sergio15.dev@gmail.com>
2024-03-10 16:30:59 -0500

    add settings for production

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import taskRoutes from './routes/';
+import path from 'path';
 
 const app = express();
 
@@ -8,5 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/tasks", taskRoutes);
+
+// Serve static files for production
+app.use(express.static(path.join(__dirname,'..','..','client','dist')));
 
 export default app;
